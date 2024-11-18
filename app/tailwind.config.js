edc177d9626ba916519062615bc9b2fc0d1d1bb4
Eric <ericfallods@protonmail.com>
2024-11-18 14:01:53 -0500

    Added Tailwind Configs

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./index.html", "./main.js", "./src/**/*.{js,ts,jsx,tsx}"],
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Adjust for your file types and structure
+  ],
   theme: {
     extend: {},
   },
